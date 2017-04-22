ad350059a2ab00d49be0df62e91fd2fd57863d4d
bheavey <bheavey95@gmail.com>
2017-04-22 12:59:10 -0400

    Update aStar.py

--- scripts/aStar.py
+++ scripts/aStar.py
@@ -343,15 +343,15 @@ def get_Path(cells): #takes a list of location tuples in the order that we wish 
 	pathHead.stamp = rospy.get_rostime()
 	pathHead.frame_id = "aStar_Path"
 	
-	waypoints = cells #Assume all cells in the path are waypoints until proven otherwise
+	#waypoints = cells #Assume all cells in the path are waypoints until proven otherwise
 
 	poses = [] #create the list to store all of the poses (waypoints) as PoseStamped objects
 
 	i = 1
 	while i < len(waypoints)-1:#we don't want to check the first or last cels
 		#if the waypoint has the same x or y as the ones in front and behind it, no turn is needed, therefore it is not a waypoint
-		if (waypoints[i-1])[0] == (waypoints[i])[0] == (waypoints[i+1])[0] or (waypoints[i-1])[1] == (waypoints[i])[1] == (waypoints[i+1])[1]:
-			waypoints.remove(waypoints[i])#remove the cell from the list of waypoints
+		if not (waypoints[i-1])[0] == (waypoints[i])[0] == (waypoints[i+1])[0] or (waypoints[i-1])[1] == (waypoints[i])[1] == (waypoints[i+1])[1]:
+			waypoints.append(waypoints[i])#remove the cell from the list of waypoints
 		i = i+1
 	print "WAYPOINTS"
 	print waypoints
