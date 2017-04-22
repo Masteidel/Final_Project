cc3499801ee68d4978777557de5c99b5fc7acbff
bheavey <bheavey95@gmail.com>
2017-04-22 12:38:19 -0400

    more bug fixing

--- scripts/aStar.py
+++ scripts/aStar.py
@@ -347,7 +347,7 @@ def get_Path(cells): #takes a list of location tuples in the order that we wish 
 
     poses = [] #create the list to store all of the poses (waypoints) as PoseStamped objects
 
-    for i in range(1,len(waypoints)-1):#we don't want to check the first or last cels
+    for i in range(1,len(waypoints)-2):#we don't want to check the first or last cels
     	#if the waypoint has the same x or y as the ones in front and behind it, no turn is needed, therefore it is not a waypoint
 		if (waypoints[i-1])[0] == (waypoints[i])[0] == (waypoints[i+1])[0] or (waypoints[i-1])[1] == (waypoints[i])[1] == (waypoints[i+1])[1]:
 			waypoints.remove(waypoints[i])#remove the cell from the list of waypoints
