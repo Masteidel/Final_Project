5d23217322da5c1d9cde58e4bbb69daa02047788
Unknown <masteidel@gmail.com>
2017-04-23 14:14:08 -0400

    Edit

--- scripts/pathFollower.py
+++ scripts/pathFollower.py
@@ -157,8 +157,8 @@ def timerCallback(event):
     pose = Pose()
 
     if map_list.frameExists('/base_link') and map_list.frameExists('/map'):
-        #map_list.waitForTransform('/base_link', '/map', rospy.Time(0), rospy.Duration(0.1))
         t = map_list.getLatestCommonTime('/base_link', '/map')
+        map_list.waitForTransform('/base_link', '/map', t, rospy.Duration(0.1))
         (position, orientation) = map_list.lookupTransform('/base_link', '/map', t) #finds the position and oriention of two objects relative to each other (hint: this returns arrays, while Pose uses lists)
         pose.position.x = position[0]
         pose.position.y = position[1]
